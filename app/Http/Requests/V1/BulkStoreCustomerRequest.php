e4f2df4ef396c99624921a3bbbed299f43448e7c
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-03-15 12:27:33 +0100

    token authentication for requests

--- app/Http/Requests/V1/BulkStoreCustomerRequest.php
+++ app/Http/Requests/V1/BulkStoreCustomerRequest.php
@@ -12,7 +12,7 @@ class BulkStoreCustomerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return request()->user()->tokenCan('create');
     }
 
     /**
